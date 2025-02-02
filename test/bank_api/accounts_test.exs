126b98a1eb99a527253f86b4fa2eaab1d51ec877
Ryan Yogan <ryanyogan@gmail.com>
2025-02-02 10:28:40 -0600

    text coverage for command validation

--- test/bank_api/accounts_test.exs
+++ test/bank_api/accounts_test.exs
@@ -10,4 +10,47 @@ defmodule BankAPI.Accounts.AccountsTest do
 
     assert {:ok, %Account{current_balance: 1_000}} = Accounts.open_account(params)
   end
+
+  test "does not dispatch command with invalid payload" do
+    params = %{
+      "initial_bad_balance" => 1_000
+    }
+
+    assert {:error, :bad_or_unknown_command} = Accounts.open_account(params)
+  end
+
+  test "returns validation errors from dispatch" do
+    params1 = %{
+      "initial_balance" => "1_000"
+    }
+
+    params2 = %{
+      "initial_balance" => -10
+    }
+
+    params3 = %{
+      "initial_balance" => 0
+    }
+
+    assert {
+             :error,
+             :command_validation_failure,
+             _cmd,
+             ["Expected INTEGER, got STRING \"1_000\", at initial_balance"]
+           } = Accounts.open_account(params1)
+
+    assert {
+             :error,
+             :command_validation_failure,
+             _cmd,
+             ["Argument must be bigger than zero"]
+           } = Accounts.open_account(params2)
+
+    assert {
+             :error,
+             :command_validation_failure,
+             _cmd,
+             ["Argument must be bigger than zero"]
+           } = Accounts.open_account(params3)
+  end
 end
